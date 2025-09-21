e67c88df8b590ceaa3830187dfaf6ac3e3b8576c
ShubhamKumar0209 <sshubhamkr0229@gmail.com>
2025-09-22 01:02:47 +0530

    Add new userRoutes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { logoutUser, refreshAccessToken, registerUser, updateUserAvatar } from "../controllers/user.controller.js";
+import { changePassWord, getCurrentUser, getUserChannelProfile, getWatchHistory, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { User } from "../models/User.models.js";
 import { loginUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 const userRouter=Router();
 
@@ -23,5 +24,18 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT,logoutUser)
 userRouter.route("/refresh-token").post(refreshAccessToken)
 userRouter.route("/updateUserAvatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+
+userRouter.route("/updateCoverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+
+userRouter.route("/changePassword").post(verifyJWT,changePassWord);
+
+userRouter.route("/getCurrentUser").get(verifyJWT,getCurrentUser)
+
+userRouter.route("/updateDetails").patch(verifyJWT,updateAccountDetails);
+//get is used since user is not updating anything
+//post is used when user is updating something
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+
+userRouter.route("/watchHistory").get(verifyJWT,getWatchHistory) 
 
 export default userRouter;
